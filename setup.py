c5ac435773ec9c56d049ba180af7a9db2a8fd810
Hajime Nakagami <nakagami@gmail.com>
2024-10-11 08:34:37 +0900

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 description = "Matplotlib sixel backend"
 setup(name="pyplotsixel",
-      version="0.2.0",
+      version="0.2.1",
       description=description,
       long_description=open('README.rst').read(),
       url="https://github.com/nakagami/pyplotsixel",
